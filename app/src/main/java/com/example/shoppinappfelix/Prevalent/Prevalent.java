8de7120de24581ea028816505940aecd021212ea
Felix ITs 04 <malikhimani21@gmail.com>
2021-08-13 13:07:23 +0530

    first commit

---
+++ app/src/main/java/com/example/shoppinappfelix/Prevalent/Prevalent.java
@@ -0,0 +1,14 @@
+package com.example.shoppinappfelix.Prevalent;
+
+
+import com.example.shoppinappfelix.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    // Paper dependency
+    // save user information at the time of login
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
